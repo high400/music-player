62e9f515e145eb18f5b5f6186b7a046b33c3249c
hang long <sapbatcheung@gmail.com>
2021-06-25 11:18:40 +0700

    new

--- App.js
+++ App.js
@@ -56,6 +56,104 @@ export default class App extends React.Component {
     volume: 1.0,
     isBuffering: false,
   };
+
+  async componentDidMount() {
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+        shouldDuckAndroid: true,
+        staysActiveInBackground: true,
+        playThroughEarpieceAndroid: true
+      })
+ 
+      this.loadAudio()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async loadAudio() {
+  const {currentIndex, isPlaying, volume} = this.state
+ 
+  try {
+    const playbackInstance = new Audio.Sound()
+    const source = {
+      uri: audioBookPlaylist[currentIndex].uri
+    }
+ 
+    const status = {
+      shouldPlay: isPlaying,
+      volume
+    }
+ 
+    playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)     
+    await playbackInstance.loadAsync(source, status, false)
+    this.setState({playbackInstance})
+    } catch (e) {
+      console.log(e)
+    }
+}
+ 
+onPlaybackStatusUpdate = status => {
+  this.setState({
+    isBuffering: status.isBuffering
+  })
+}
+
+handlePlayPause = async () => {
+    const { isPlaying, playbackInstance } = this.state
+    isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
+ 
+    this.setState({
+      isPlaying: !isPlaying
+    })
+  }
+ 
+    handlePreviousTrack = async () => {
+    let { playbackInstance, currentIndex } = this.state
+    if (playbackInstance) {
+      await playbackInstance.unloadAsync()
+      currentIndex < audioBookPlaylist.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
+      this.setState({
+        currentIndex
+      })
+      this.loadAudio()
+    }
+  }
+ 
+  handleNextTrack = async () => {
+    let { playbackInstance, currentIndex } = this.state
+    if (playbackInstance) {
+      await playbackInstance.unloadAsync()
+      currentIndex < audioBookPlaylist.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
+      this.setState({
+        currentIndex
+      })
+      this.loadAudio()
+    }
+  }
+
+  renderFileInfo() {
+    const { playbackInstance, currentIndex } = this.state
+    return playbackInstance ? (
+      <View style={styles.trackInfo}>
+        <Text style={[styles.trackInfoText, styles.largeText]}>
+          {audioBookPlaylist[currentIndex].title}
+        </Text>
+        <Text style={[styles.trackInfoText, styles.smallText]}>
+          {audioBookPlaylist[currentIndex].author}
+        </Text>
+        <Text style={[styles.trackInfoText, styles.smallText]}>
+          {audioBookPlaylist[currentIndex].source}
+        </Text>
+      </View>
+    ) : null
+  }
+ 
+
   render() {
     return (
       <View style={styles.container}>
@@ -66,25 +164,27 @@ export default class App extends React.Component {
           }}
         />
         <View style={styles.controls}>
-          <TouchableOpacity style={styles.control}>
+          <TouchableOpacity style={styles.control} onPress={this.handlePreviousTrack} >
             <AntDesign name="banckward" size={48} color="#444" />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.control} onPress={() => alert("")}>
+          <TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
             {this.state.isPlaying ? (
               <Ionicons name="ios-pause" size={48} color="#444" />
             ) : (
               <Ionicons name="ios-play-circle" size={48} color="#444" />
             )}
           </TouchableOpacity>
-          <TouchableOpacity style={styles.control}>
+          <TouchableOpacity style={styles.control} onPress={this.handleNextTrack} >
             <AntDesign name="forward" size={48} color="#444" />
           </TouchableOpacity>
         </View>
+        {this.renderFileInfo()}
       </View>
     );
   }
 }
 
+// update the Stylesheet object 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -96,10 +196,25 @@ const styles = StyleSheet.create({
     width: 250,
     height: 250
   },
-  controls: {
-    flexDirection: 'row'
+  trackInfo: {
+    padding: 40,
+    backgroundColor: '#fff'
+  },
+  trackInfoText: {
+    textAlign: 'center',
+    flexWrap: 'wrap',
+    color: '#550088'
+  },
+  largeText: {
+    fontSize: 22
+  },
+  smallText: {
+    fontSize: 16
   },
   control: {
     margin: 20
+  },
+  controls: {
+    flexDirection: 'row'
   }
 })
