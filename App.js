4cc973451b4817e4ddfcff9feb0052df587b83c6
hang long <sapbatcheung@gmail.com>
2021-06-25 10:34:04 +0700

    new

--- App.js
+++ App.js
@@ -1,14 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, TouchableOpacity, View, Image, Text } from "react-native";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
+import { Audio } from 'expo-av'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const audioBookPlaylist = [
+  {
+    title: "Hamlet - Act I",
+    author: "William Shakespeare",
+    source: "Librivox",
+    uri: "https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3",
+    imageSource:
+      "http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg",
+  },
+  {
+    title: "Hamlet - Act II",
+    author: "William Shakespeare",
+    source: "Librivox",
+    uri: "https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3",
+    imageSource:
+      "http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg",
+  },
+  {
+    title: "Hamlet - Act III",
+    author: "William Shakespeare",
+    source: "Librivox",
+    uri: "http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3",
+    imageSource:
+      "http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg",
+  },
+  {
+    title: "Hamlet - Act IV",
+    author: "William Shakespeare",
+    source: "Librivox",
+    uri: "https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3",
+    imageSource:
+      "http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg",
+  },
+  {
+    title: "Hamlet - Act V",
+    author: "William Shakespeare",
+    source: "Librivox",
+    uri: "https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3",
+    imageSource:
+      "http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg",
+  },
+];
+
+
+export default class App extends React.Component {
+  state = {
+    isPlaying: false,
+    playbackInstance: null,
+    currentIndex: 0,
+    volume: 1.0,
+    isBuffering: false,
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.albumCover}
+          source={{
+            uri: "http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg",
+          }}
+        />
+        <View style={styles.controls}>
+          <TouchableOpacity style={styles.control}>
+            <AntDesign name="banckward" size={48} color="#444" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.control} onPress={() => alert("")}>
+            {this.state.isPlaying ? (
+              <Ionicons name="ios-pause" size={48} color="#444" />
+            ) : (
+              <Ionicons name="ios-play-circle" size={48} color="#444" />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.control}>
+            <AntDesign name="forward" size={48} color="#444" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +90,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  albumCover: {
+    width: 250,
+    height: 250
+  },
+  controls: {
+    flexDirection: 'row'
+  },
+  control: {
+    margin: 20
+  }
+})
